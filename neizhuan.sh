2cc0790330b64c9036f696b770a9c3ce960db826
Guihu88 <137459673+Guihu88@users.noreply.github.com>
2023-06-23 15:37:57 +0800

    Update neizhuan.sh

--- neizhuan.sh
+++ neizhuan.sh
@@ -1,15 +1,7 @@
 #!/bin/bash
 
-# 允许SSH连接的流量通过
-iptables -A INPUT -p tcp --dport 22 -j ACCEPT
-
 read -p "请输入目标内网IP地址: " destination_ip
 read -p "请输入本机内网IP地址: " local_ip
-read -p "请输入限速大小（例如50M）: " bandwidth
-
-# 安装tc和iptables-persistent
-apt-get update
-apt-get install -y tc iptables-persistent
 
 # 清空现有的规则和链
 iptables -F
@@ -31,16 +23,9 @@ iptables -t nat -A PREROUTING -p udp --dport 1025:65535 -j DNAT --to-destination
 iptables -t nat -A POSTROUTING -p tcp -d "$destination_ip" --dport 1025:65535 -j SNAT --to-source "$local_ip"
 iptables -t nat -A POSTROUTING -p udp -d "$destination_ip" --dport 1025:65535 -j SNAT --to-source "$local_ip"
 
-# 限速设置
-tc qdisc add dev eth0 root tbf rate "$bandwidth" burst 10kb latency 70ms
-
 # 保存规则
 mkdir -p /etc/iptables/
 iptables-save > /etc/iptables/rules.v4
-
-# 保存tc规则
-tc qdisc save dev eth0 root > /etc/systemd/system/tc.service
-systemctl enable tc.service
 
 # 安装iptables-persistent以永久保存规则
 apt-get update
